d6e1f5a20b4cc2131143e28cd90c8492c2e64563
pidoubleyou <pidoubleyou@arcor.de>
2020-11-06 11:15:15 +0100

    initial tracking service

---
+++ tracking/src/test/java/pw/chaos/tracking/TrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.chaos.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
